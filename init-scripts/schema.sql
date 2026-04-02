ab16f4dfa0cc606dc0d4259e43735637c06b8247
Kiril Bogza <sobacika1999@gmail.com>
2026-04-02 13:32:33 +0200

    fix: update schema sql syntax
    MySQL -> PostgreSQL

--- init-scripts/schema.sql
+++ init-scripts/schema.sql
@@ -1,32 +1,28 @@
-CREATE TABLE
-    users (
-        user_id INT PRIMARY KEY AUTO_INCREMENT,
-        username VARCHAR(50) NOT NULL UNIQUE,
-        password_hash VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    );
+CREATE TABLE users (
+    user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-CREATE TABLE
-    code_snippets (
-        snippet_id INT PRIMARY KEY AUTO_INCREMENT,
-        title VARCHAR(100) NOT NULL,
-        code TEXT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    );
+CREATE TABLE code_snippets (
+    snippet_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    code TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-create table
-    comments (
-        comment_id INT PRIMARY KEY AUTO_INCREMENT,
-        user_id INT NOT NULL,
-        snippet_id INT NOT NULL,
-        content TEXT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
-        FOREIGN KEY (snippet_id) REFERENCES code_snippets (snippet_id) ON DELETE CASCADE
-    );
+CREATE TABLE comments (
+    comment_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INT NOT NULL,
+    snippet_id INT NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
+    FOREIGN KEY (snippet_id) REFERENCES code_snippets (snippet_id) ON DELETE CASCADE
+);
 
-create table
-    code_languages (
-        language_id INT PRIMARY KEY AUTO_INCREMENT,
-        name VARCHAR(50) NOT NULL UNIQUE
-    );
+CREATE TABLE code_languages (
+    language_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
