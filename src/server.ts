b329cda7f86965242f07999fb5b7a4cf0a698e59
devinderstockholm3219gmailcom <devinder.stockholm3219@gmail.com>
2026-04-02 14:14:21 +0200

    feat: add server entry point

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import { buildApp } from "./app.js";
+
+const app = buildApp();
+const PORT = parseInt(process.env.PORT ?? "3000");
+
+app.listen({ port: PORT, host: "0.0.0.0" }, (err) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+});
